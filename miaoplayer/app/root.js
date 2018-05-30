0248c3c64087238ace32ab87d6fefb971b133425
catsugar <773817248@qq.com>
2018-05-30 22:41:15 +0800

    333

---
+++ miaoplayer/app/root.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import Header from './components/header';
+var createClass = require('create-react-class');
+let Root=createClass({
+	render() {
+	  return(
+		<Header/>	
+	  );
+	}
+});
+export default Root;
