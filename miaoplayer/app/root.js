edecf8063d681b20a18db89758fbbe1d991b4607
catsugar <773817248@qq.com>
2018-06-02 22:54:02 +0800

    11

--- miaoplayer/app/root.js
+++ miaoplayer/app/root.js
@@ -1,11 +1,39 @@
 
 import React from 'react';
 import Header from './components/header';
+import Progress from './components/progress';
+
 var createClass = require('create-react-class');
 let Root=createClass({
+	getInitialState(){
+		return {
+			progress:"-"
+		}
+	},
+	componentDidMount(){
+		$('#player').jPlayer({
+			ready:function(){
+			  $(this).jPlayer('setMedia',{
+			  	mp3:''
+			  }).jPlayer('play');	
+			},
+			supplied: 'mp3',
+			wmode:'window'
+		});
+		$('#player').bind($.jPlayer.event.timeupdate,(e)=>{
+           this.setState({
+           	  progress: Math.round(e.jPlayer.status.currentTime)
+           })
+		});
+	}
 	render() {
 	  return(
-		<Header/>	
+	  	<div>
+			<Header/>	
+			<Progress
+	           progress={this.state.progress}
+			></Progress>
+		</div>
 	  );
 	}
 });
