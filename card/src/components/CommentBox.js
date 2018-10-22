8f25aa3a431277e0fc3dd83abba121d8313c6450
catsugar <773817248@qq.com>
2018-10-23 00:20:29 +0800

    111

--- card/src/components/CommentBox.js
+++ card/src/components/CommentBox.js
@@ -3,34 +3,27 @@ import './CommentBox.css';
 class CommentBox extends React.Component{
 	constructor(props){
 		super(props)
-		/*this.state={
-			value:""
-		}
-		this.handleChange=this.handleChange.bind(this)*/
 		this.handleSubmit=this.handleSubmit.bind(this)
 	}
-	handleChange(event){
-		this.setState({
-			value: event.target.value
-		})
-	}
 	handleSubmit (event){
-		//alert(event.target.value)
+		this.props.onAddComment(this.textInput.value)
 		event.preventDefault()
-		alert(event.textInput.value)
 	}
 	render(){
 		return (
-		  <form className="commentbox" onSubmit={this.handleSumbit}>
+		  <form className="commentbox" onSubmit={this.handleSubmit}>
 		    <div className="comment">
 		      <div>留言内容:</div>
 		      <input 
 		      type="text"  
 		      className="control" 
-		      placeholder="请输入内容"  ref={(textInput) => {this.textInput = textInput}}/>
+		      placeholder="请输入内容"  
+		      ref={(textInput) => {this.textInput = textInput}}/>
 		    </div>  
+		    <span className="number" >已有{this.props.commentLength}条评论</span>
 		    <button type="submit" className="btn" >留言</button>
-		  </form>
+		    <hr/>
+		   </form>
 
 		)
 	}
--- card/src/components/CommentList.js
+++ card/src/components/CommentList.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import './CommentList.css';
-import CommentBox from './CommentBox';
-class CommentList extends React.Component{
-	constructor(props){
-		super(props)
-		this.state={
-			comments:["呵呵哈哈哈或"]
-		}
-	}
-	render(){
-		return (
-		  <div className="commentList">
-		    <CommentBox />
+
+const CommentList =({ comments }) =>{
+	return (
+		<div className="commentList">
 		    <div>评论列表:</div>
 		    <ul className="list">
-		      {comments.map((comment,index) =>
-                <li key={index} >{comment} </li>
-		      )}
-		    </ul>	    
-		  </div>
+		      {comments.map((item,index) =>
+	                      <li key={index}>{item}</li>
+		                )}
 
-		)
-	}
+		    </ul>	    
+		</div>
+	)
 }
 export default CommentList
