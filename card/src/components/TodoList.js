aba6e14ac29aa12056597fd78155cf3369d92027
catsugar <773817248@qq.com>
2018-11-07 13:02:10 +0800

    111

--- card/src/components/TodoList.js
+++ card/src/components/TodoList.js
@@ -13,23 +13,35 @@ class TodoList extends React.Component{
     }
     addEvent (){
       this.setState({
-        events: [...this.state.events,inputValue]
+        events: [...this.state.events, this.state.inputValue],
+        inputValue: ''
       })
+      this.inputChange=this.inputChange.bind(this);
+      this.addEvent=this.addEvent.bind(this);
 	}
     inputChange(e){
       this.setState({
-        inputValue: e.target.value()
+        inputValue: e.target.value
       })
+    }
+    itemClick(index){
+        console.log(index);
+        const events= [...this.state.events];
+        events.splice(index,1)
+        this.setState({
+            events:events
+        })
+
     }
     render(){
         return (
             <div className="todolist">
                 <div>记录每天要完成的事:</div>
-                <input className="control" onChange={this.inputChange} ref={(textInput) => {this.textInput = textInput}} placeholder="请输入内容" />
-                <button className="btn" onClick={this.addEvent.bind(this)}>记录</button>
+                <input value={this.state.inputValue} className="control" onChange={this.inputChange} placeholder="请输入内容" />
+                <button className="btn" onClick={this.addEvent}>记录</button>
                 <ul className="list">
                   {this.state.events.map((item,index) =>
-                          <li key={index}>{item}</li>
+                          <li key={index} onClick={this.itemClick.bind(this,index)}>{item}</li>
                         )}
 		        </ul>	 
             </div>
